967016cd25e1fd45be6e22ff9cc2aa1e39a3d6d0
Michael Womer <mwomer27@gmail.com>
2025-04-17 16:17:27 -0400

    spring backend initialized

---
+++ backend/restaurant-server/src/test/java/com/cd/restaurant_server/RestaurantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.restaurant_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
