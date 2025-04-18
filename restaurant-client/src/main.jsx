e5df2da409d3b3fa52a818d237e2110919bfd3bc
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-04-18 18:36:57 -0400

    Update main.jsx

--- restaurant-client/src/main.jsx
+++ restaurant-client/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createReservationPage } from './reservation.js';
+
+createReservationPage('reservation');
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
