025459c216f801b1e2f03e08e729c33989ae860a
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-04-18 18:49:24 -0400

    Update menu.js

--- restaurant-client/src/pages/menu.js
+++ restaurant-client/src/pages/menu.js
@@ -15,13 +15,13 @@ export function createMenuPage(containerId) {
           name: 'Avocado Toast',
           description: 'Sourdough topped with smashed avocado, chili flakes & poached egg.',
           price: '$12',
-          image: 'https://source.unsplash.com/400x300/?avocado-toast'
+          image: 'https://www.mediterraneanliving.com/wp-content/uploads/2018/04/image4.jpg'
         },
         {
           name: 'Berry Pancakes',
           description: 'Fluffy pancakes stacked with fresh berries and maple syrup.',
           price: '$10',
-          image: 'https://source.unsplash.com/400x300/?pancakes'
+          image: 'https://chocolatewithgrace.com/wp-content/uploads/2023/03/Berry-Pancakes-CWG-7-1-of-1-scaled.jpg'
         }
       ],
       Dinner: [
@@ -29,13 +29,13 @@ export function createMenuPage(containerId) {
           name: 'Grilled Salmon',
           description: 'Fresh salmon with garlic herb butter and seasonal vegetables.',
           price: '$22',
-          image: 'https://source.unsplash.com/400x300/?grilled-salmon'
+          image: 'https://hips.hearstapps.com/hmg-prod/images/how-to-grill-salmon-recipe1-1655870645.jpg?crop=0.6666666666666667xw:1xh;center,top&resize=1200:*'
         },
         {
           name: 'Truffle Pasta',
           description: 'Creamy fettuccine with wild mushrooms and shaved truffle.',
           price: '$19',
-          image: 'https://source.unsplash.com/400x300/?truffle-pasta'
+          image: 'https://www.sweetteaandthyme.com/wp-content/uploads/2024/02/truffle-mushroom-pasta-close-up.jpg'
         }
       ],
       Drinks: [
@@ -43,13 +43,13 @@ export function createMenuPage(containerId) {
           name: 'Espresso Martini',
           description: 'A perfect blend of espresso, vodka & coffee liqueur.',
           price: '$14',
-          image: 'https://source.unsplash.com/400x300/?espresso-martini'
+          image: 'https://www.pamperedchef.ca/iceberg/com/recipe/2132836-lg.jpg'
         },
         {
           name: 'Cucumber Cooler',
           description: 'Gin-based cocktail with cucumber, mint & lime.',
           price: '$12',
-          image: 'https://source.unsplash.com/400x300/?cocktail'
+          image: 'https://i0.wp.com/www.imbored-letsgo.com/wp-content/uploads/2016/05/Gin-Cucumber-Cooler.jpg?ssl=1'
         }
       ]
     };
